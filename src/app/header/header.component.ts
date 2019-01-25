325eeadc479957de39846d7a5f10310ea8b624ab
Ralph Perdomo <ralph@prdmo.com>
2019-01-25 08:50:51 -0800

    add masthead nav items

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  mastHeadings: string[] = [ 'World', 'U.S.', 'Politics', 'N.Y.', 'Business', 'Opinion', 'Tech', 'Science', 'Health', 'Sports', 'Arts', 'Books', 'Style', 'Food', 'Travel', 'Magazine', 'T Magazine', 'Real Estate', 'Video'];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
