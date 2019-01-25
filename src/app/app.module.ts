7745bc7442765b81d3775cfd69a80838ab673269
Ralph Perdomo <ralph@prdmo.com>
2019-01-25 09:35:02 -0800

    add nav and search icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { PancakesComponent } from './pancakes/pancakes.component';
+import { SearchComponent } from './search/search.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PancakesComponent,
+    SearchComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pancakes/pancakes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pancakes',
+  templateUrl: './pancakes.component.html',
+  styleUrls: ['./pancakes.component.css']
+})
+export class PancakesComponent {
+
+
+
+}
