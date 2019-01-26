63a0b95006119a0a64656e2e0703f5130e68ef80
Ralph Perdomo <ralph@prdmo.com>
2019-01-25 16:41:07 -0800

    complete styling on briefings section and confirm API pulls data

--- src/app/briefings/briefings.component.ts
+++ src/app/briefings/briefings.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { BriefingsArticle } from '../models/briefings-article';
 import { USMarkets } from '../stock-getter';
 
-// private tickers = ['DJIA','GSPC'];
-
-
-
-
 @Component({
   selector: 'app-briefings',
   templateUrl: './briefings.component.html',
@@ -15,23 +10,24 @@ import { USMarkets } from '../stock-getter';
 
 export class BriefingsComponent implements OnInit {
 
+
   ngOnInit() {
-    console.log('hi')
     const stocks: USMarkets = new USMarkets();
-    stocks.getStocks('GSPC').then(function(results) {
+
+    stocks.getStocks('gspc').then(function(results) {
       console.log(results)
     });
+
+    stocks.getStocks('djia').then(function(results) {
+      console.log(results)
+    });
+
   }
-
-  
-
 
   artcilesArray: BriefingsArticle[] = [
     new BriefingsArticle(`Listen to 'The Daily'`, 'One country, two presidents: explaining the crisis in Venezuela.'),
     new BriefingsArticle(`In the 'At War' Newsletter`,'The relics of war left behind in an Afghan clinic.'),
     new BriefingsArticle('The Neediest Cases Fund','Born with a danerous infection, a daughter needs close care.')
   ];  
-
-
 
 }
