274f705ac098afa484d3fbcb3b75ba23cfa1d36f
Ralph Perdomo <ralph@prdmo.com>
2019-01-25 16:26:16 -0800

    cleaned market data from API call

--- src/app/stock-getter.ts
+++ src/app/stock-getter.ts
@@ -3,12 +3,12 @@ export class USMarkets {
 
   getStocks(ticker) {
     return new Promise((resolve, reject) => {
-      const url=`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=5min&apikey=${this.api}`;
+      const url=`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&interval=5min&apikey=${this.api}`;
       const request: XMLHttpRequest = new XMLHttpRequest();
       
       request.onload = function() {
         if (this.status === 200) {
-          resolve(JSON.parse(request.response))
+          resolve(JSON.parse(request.response.replace(/(\d\d[.]\s)|%/g, ''))["Global Quote"])
         } else {
           reject(Error(request.statusText))
           }
@@ -22,23 +22,3 @@ export class USMarkets {
 
 
 }
-
-
-// var data = null;
-
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//   if (this.readyState === 4) {
-//     console.log(this.responseText);
-//   }
-// });
-
-// xhr.open("GET", "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GSPC&interval=5min&apikey=E8E527S2C4TU78JR");
-// xhr.setRequestHeader("cache-control", "no-cache");
-// xhr.setRequestHeader("Postman-Token", "19657a9b-7c89-490d-9373-24d42d596cb2");
-
-// xhr.send(data);
-
-
